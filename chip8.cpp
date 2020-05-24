7ce19bc80af1128bf8106200f1ba668438ad0283
Doğaç Eldenk <dogacel@gmail.com>
2020-05-24 14:39:29 +0300

    Bug fixes

--- chip8.cpp
+++ chip8.cpp
@@ -1,290 +1,257 @@
 #include "chip8.h"
+
+#include <time.h>
+
 #include <cstdlib>
 #include <iostream>
 
-void CHIP8::init()
-{
+void CHIP8::init() {
     REG_PC = 0x200;
     in = 0x0;
     REG_I = 0x0;
     REG_SP = 0x0;
-
     for (int i = 0; i < 80; ++i)
         MEM[i] = fontset[i];
 
     srand(time(NULL));
 }
 
-void CHIP8::tick()
-{
+void CHIP8::tick() {
     in = MEM[REG_PC + 1] | (MEM[REG_PC] << 8);
-    // printf("0x%x\n", in);
+#ifdef DEBUG
+    printf("In: 0x%x\n", in);
+#endif
     bool res = 0;
     unsigned char Vx = (in >> 8) & 0x000F;
     unsigned char Vy = (in >> 4) & 0x000F;
-    switch (in >> 12)
-    {
-    case 0x0:
-        if (in == 0x00E0)
-        {
-            // CLS
-            cls();
-        }
-        else if (in == 0x00EE)
-        {
-            // RET
-            REG_SP -= 1;
-            REG_PC = STACK[REG_SP];
-        }
-        else
-        {
-            // JUMP to nnn
-            printf("Error !");
-            REG_PC = (in && 0xFFF) - 2;
-        }
-        break;
-    case 0x1:
-        // JP addr
-        REG_PC = (in & 0x0FFF) - 2;
-        break;
-    case 0x2:
-        // CALL addr
-        STACK[REG_SP] = REG_PC;
-        REG_SP += 1;
-        REG_PC = (in & 0x0FFF) - 2;
-        break;
-    case 0x3:
-        // SE Vx, byte
-        if (REG_V[(in >> 8) & 0x000F] == (in & 0x00FF))
-        {
-            REG_PC += 2;
-        }
-        break;
-    case 0x4:
-        // SNE Vx, byte
-        if (REG_V[(in >> 8) & 0x000F] != (in & 0x00FF))
-        {
-            REG_PC += 2;
-        }
-
-        break;
-    case 0x5:
-        // SE Vx, Vy
-        if (REG_V[(in >> 8) & 0x000F] == REG_V[(in >> 4) & 0x000F])
-        {
-            REG_PC += 2;
-        }
-        break;
-    case 0x6:
-        // LD Vx, byte
-        REG_V[(in >> 8) & 0x000F] = in & 0x00FF;
-        break;
-    case 0x7:
-        // ADD Vx, byte
-        REG_V[(in >> 8) & 0x000F] += in & 0x00FF;
-        break;
-    case 0x8:
-        switch (in & 0x000F)
-        {
+    switch (in >> 12) {
         case 0x0:
-            // LD Vx, Vy
-            REG_V[Vx] = REG_V[Vy];
+            if (in == 0x00E0) {
+                // CLS
+                cls();
+            } else if (in == 0x00EE) {
+                // RET
+                REG_SP -= 1;
+                REG_PC = STACK[REG_SP];
+            } else {
+                // JUMP to nnn
+                printf("Error !");
+                REG_PC = (in && 0xFFF) - 2;
+            }
             break;
         case 0x1:
-            // OR Vx, Vy
-            REG_V[Vx] |= REG_V[Vy];
+            // JP addr
+            REG_PC = (in & 0x0FFF) - 2;
             break;
         case 0x2:
-            // AND Vx, Vy
-            REG_V[Vx] &= REG_V[Vy];
+            // CALL addr
+            STACK[REG_SP] = REG_PC;
+            REG_SP += 1;
+            REG_PC = (in & 0x0FFF) - 2;
             break;
         case 0x3:
-            // XOR Vx, Vy
-            REG_V[Vx] ^= REG_V[Vy];
+            // SE Vx, byte
+            if (REG_V[(in >> 8) & 0x000F] == (in & 0x00FF)) {
+                REG_PC += 2;
+            }
             break;
         case 0x4:
-            // ADD Vx, Vy
-            if (REG_V[Vx] > 0xFF - REG_V[Vy])
-            {
-                REG_V[0xF] = 1;
+            // SNE Vx, byte
+            if (REG_V[(in >> 8) & 0x000F] != (in & 0x00FF)) {
+                REG_PC += 2;
             }
-            else
-            {
-                REG_V[0xF] = 0;
-            }
-            REG_V[Vx] = (REG_V[Vy] + REG_V[Vx]) & 0xFF;
+
             break;
         case 0x5:
-            // SUB Vx, Vy
-            REG_V[0xF] = REG_V[Vx] > REG_V[Vy] ? 1 : 0;
-            REG_V[Vx] -= REG_V[Vy];
+            // SE Vx, Vy
+            if (REG_V[Vx] == REG_V[Vy]) {
+                REG_PC += 2;
+            }
             break;
         case 0x6:
-            // SHR Vx {, Vy}
-            REG_V[0xF] = REG_V[Vx] & 0x1;
-            REG_V[Vx] >>= 1;
+            // LD Vx, byte
+            REG_V[Vx] = in & 0xFF;
             break;
         case 0x7:
-            // SUBN Vx, Vy
-            REG_V[0xF] = REG_V[Vy] > REG_V[Vx] ? 1 : 0;
-            REG_V[Vx] = REG_V[Vy] - REG_V[Vx];
+            // ADD Vx, byte
+            REG_V[Vx] += in & 0xFF;
             break;
-        case 0xE:
-            // SHL Vx {, Vy}
-            REG_V[0xF] = REG_V[Vx] >> 7;
-            REG_V[Vx] <<= 1;
-            break;
-        }
-        break;
-    case 0x9:
-        // SNE Vx, Vy
-        if (REG_V[(in >> 8) & 0x000F] != REG_V[(in >> 4) & 0x000F])
-        {
-            REG_PC += 2;
-        }
-        break;
-    case 0xA:
-        // LD I, addr
-        REG_I = in & 0x0FFF;
-        break;
-    case 0xB:
-        // JP V0, addr
-        REG_PC = (in & 0x0FFF) + REG_V[0x0] - 2;
-        break;
-    case 0xC:
-        // RND Vx, byte
-        // printf("RAND: %d\n", rand() & in & 0xFF);
-        REG_V[Vx] = rand() & in & 0xFF;
-        break;
-    case 0xD:
-        // DRW Vx, Vy, nibble
-        // printf("Drawing %d to %d : %d\n", MEM[REG_I], REG_V[Vx], REG_V[Vy]);
-        res = 0;
-        for (unsigned char n = 0; n < (in & 0xF); n++)
-        {
-            res |= draw(REG_V[Vx], REG_V[Vy] + n, MEM[REG_I + n]);
-        }
-        if (res)
-        {
-            REG_V[0xF] = 1;
-        }
-        else
-        {
-            REG_V[0xF] = 0;
-        }
-        break;
-    case 0xE:
-        switch (in & 0xFF)
-        {
-        case 0x9E:
-            // SKP Vx
-            if (keys[REG_V[Vx]])
-            {
-                REG_PC += 2;
-            }
-            break;
-        case 0xA1:
-            // SKNP Vx
-            if (!keys[REG_V[Vx]])
-            {
-                REG_PC += 2;
-            }
-            break;
-        }
-        break;
-    case 0xF:
-        switch (in & 0xFF)
-        {
-        case 0x07:
-            // LD Vx, DT
-            REG_V[Vx] = REG_DT / 8;
-            break;
-        case 0x0A:
-            // LD Vx, K
-            for (unsigned char i = 0; i < 0xF; i++)
-            {
-                if (keys[i])
-                {
-                    REG_PC += 2;
+        case 0x8:
+            switch (in & 0x000F) {
+                case 0x0:
+                    // LD Vx, Vy
+                    REG_V[Vx] = REG_V[Vy];
                     break;
-                }
+                case 0x1:
+                    // OR Vx, Vy
+                    REG_V[Vx] |= REG_V[Vy];
+                    break;
+                case 0x2:
+                    // AND Vx, Vy
+                    REG_V[Vx] &= REG_V[Vy];
+                    break;
+                case 0x3:
+                    // XOR Vx, Vy
+                    REG_V[Vx] ^= REG_V[Vy];
+                    break;
+                case 0x4:
+                    // ADD Vx, Vy
+                    if (REG_V[Vx] > 0xFF - REG_V[Vy]) {
+                        REG_V[0xF] = 1;
+                    } else {
+                        REG_V[0xF] = 0;
+                    }
+                    REG_V[Vx] = REG_V[Vy] + REG_V[Vx];
+                    break;
+                case 0x5:
+                    // SUB Vx, Vy
+                    REG_V[0xF] = REG_V[Vx] > REG_V[Vy] ? 1 : 0;
+                    REG_V[Vx] -= REG_V[Vy];
+                    break;
+                case 0x6:
+                    // SHR Vx {, Vy}
+                    REG_V[0xF] = REG_V[Vx] & 0x1;
+                    REG_V[Vx] >>= 1;
+                    break;
+                case 0x7:
+                    // SUBN Vx, Vy
+                    REG_V[0xF] = REG_V[Vy] > REG_V[Vx] ? 1 : 0;
+                    REG_V[Vx] = REG_V[Vy] - REG_V[Vx];
+                    break;
+                case 0xE:
+                    // SHL Vx {, Vy}
+                    REG_V[0xF] = REG_V[Vx] >> 7;
+                    REG_V[Vx] <<= 1;
+                    break;
             }
-            REG_PC -= 2;
             break;
-        case 0x15:
-            // LD DT, Vx
-            REG_DT = REG_V[Vx] * 8;
+        case 0x9:
+            // SNE Vx, Vy
+            if (REG_V[(in >> 8) & 0x000F] != REG_V[(in >> 4) & 0x000F]) {
+                REG_PC += 2;
+            }
             break;
-        case 0x18:
-            // LD ST, Vx
-            REG_ST = REG_V[Vx];
+        case 0xA:
+            // LD I, addr
+            REG_I = in & 0x0FFF;
             break;
-        case 0x1E:
-            // ADD I, Vx
-            if (REG_I + REG_V[Vx] > 0xFFF)
-            {
+        case 0xB:
+            // JP V0, addr
+            REG_PC = (in & 0x0FFF) + REG_V[0x0] - 2;
+            break;
+        case 0xC:
+            // RND Vx, byte
+            // printf("RAND: %d\n", rand() & in & 0xFF);
+            REG_V[Vx] = rand() & in & 0xFF;
+            break;
+        case 0xD:
+// DRW Vx, Vy, nibble
+#ifdef DEBUG
+            printf("Drawing %d to %d : %d\n", MEM[REG_I], REG_V[Vx], REG_V[Vy]);
+#endif
+            res = 0;
+            for (unsigned char n = 0; n < (in & 0xF); n++) {
+                res |= draw(REG_V[Vx], REG_V[Vy] + n, MEM[REG_I + n]);
+            }
+            if (res) {
                 REG_V[0xF] = 1;
-            }
-            else
-            {
+            } else {
                 REG_V[0xF] = 0;
             }
-            REG_I += REG_V[Vx];
             break;
-        case 0x29:
-            // LD F, Vx
-            REG_I = REG_V[Vx] * 5;
-            break;
-        case 0x33:
-            // LD B, Vx
-            MEM[REG_I] = REG_V[Vx] / 100;
-            MEM[REG_I + 1] = (REG_V[Vx] % 100) / 10;
-            MEM[REG_I + 2] = REG_V[Vx] % 10;
-            break;
-        case 0x55:
-            // LD [I], Vx
-            for (unsigned char i = 0x0; i <= Vx; i++)
-            {
-                MEM[REG_I + i] = REG_V[i];
+        case 0xE:
+            switch (in & 0xFF) {
+                case 0x9E:
+                    // SKP Vx
+                    if (keys[REG_V[Vx]]) {
+                        REG_PC += 2;
+                    }
+                    break;
+                case 0xA1:
+                    // SKNP Vx
+                    if (!keys[REG_V[Vx]]) {
+                        REG_PC += 2;
+                    }
+                    break;
             }
             break;
-        case 0x65:
-            //  LD Vx, [I]
-            for (unsigned char i = 0x0; i <= Vx; i++)
-            {
-                REG_V[i] = MEM[REG_I + i];
+        case 0xF:
+            switch (in & 0xFF) {
+                case 0x07:
+                    // LD Vx, DT
+                    REG_V[Vx] = REG_DT / 8;
+                    break;
+                case 0x0A:
+                    // LD Vx, K
+                    for (unsigned char i = 0; i <= 0xF; i++) {
+                        if (keys[i]) {
+                            REG_V[Vx] = i;
+                            REG_PC += 2;
+                            break;
+                        }
+                    }
+                    REG_PC -= 2;
+                    break;
+                case 0x15:
+                    // LD DT, Vx
+                    REG_DT = REG_V[Vx] * 8;
+                    break;
+                case 0x18:
+                    // LD ST, Vx
+                    REG_ST = REG_V[Vx];
+                    break;
+                case 0x1E:
+                    // ADD I, Vx
+                    REG_I += REG_V[Vx];
+                    break;
+                case 0x29:
+                    // LD F, Vx
+                    REG_I = REG_V[Vx] * 5;
+                    break;
+                case 0x33:
+                    // LD B, Vx
+                    MEM[REG_I] = REG_V[Vx] / 100;
+                    MEM[REG_I + 1] = (REG_V[Vx] % 100) / 10;
+                    MEM[REG_I + 2] = REG_V[Vx] % 10;
+                    break;
+                case 0x55:
+                    // LD [I], Vx
+                    for (unsigned char i = 0x0; i <= Vx; i++) {
+                        MEM[REG_I + i] = REG_V[i];
+                    }
+                    break;
+                case 0x65:
+                    //  LD Vx, [I]
+                    for (unsigned char i = 0x0; i <= Vx; i++) {
+                        REG_V[i] = MEM[REG_I + i];
+                    }
+                    break;
             }
             break;
-        }
-        break;
-    default:
-        printf("Unknown instruction: 0x%X\n", in);
+        default:
+            printf("Unknown instruction: 0x%X\n", in);
     }
 
     REG_PC += 2;
 
-    if (REG_DT > 0)
-    {
+    if (REG_DT > 0) {
         REG_DT--;
     }
 
-    if (REG_ST > 0)
-    {
-        if (REG_ST == 1)
-        {
+    if (REG_ST > 0) {
+        if (REG_ST == 1) {
             printf("BEEP\n");
         }
         REG_ST--;
     }
 }
 
-bool CHIP8::draw(unsigned char x, unsigned char y, unsigned char sprite)
-{
+bool CHIP8::draw(unsigned char x, unsigned char y, unsigned char sprite) {
+    x &= 63;
+    y &= 31;
     bool retval = 0;
-    while (sprite != 0x0)
-    {
-        if (gfx[x + y * 64] & (sprite >> 7) & 0x1)
-        {
+    while (sprite != 0x0) {
+        if (gfx[x + y * 64] & (sprite >> 7) & 0x1) {
             retval = 1;
         }
         gfx[x + y * 64] ^= (sprite >> 7) & 0x1;
@@ -294,19 +261,15 @@ bool CHIP8::draw(unsigned char x, unsigned char y, unsigned char sprite)
     return retval;
 }
 
-void CHIP8::cls()
-{
-    for (unsigned short i = 0; i < 64 * 32; i++)
-    {
+void CHIP8::cls() {
+    for (unsigned short i = 0; i < 64 * 32; i++) {
         gfx[i] = 0x0;
     }
 }
 
 using namespace std;
 
-void CHIP8::loadApp(const char *filename)
-{
-
+void CHIP8::loadApp(const char *filename) {
     FILE *pFile = fopen(filename, "rb");
 
     // Check file size
@@ -318,15 +281,12 @@ void CHIP8::loadApp(const char *filename)
 
     size_t result = fread(buffer, 1, lSize, pFile);
 
-    for (size_t i = 0; i < result; i++)
-    {
+    for (size_t i = 0; i < result; i++) {
         MEM[i + 512] = buffer[i];
     }
 
-    for (int i = 0; i < 128; i++)
-    {
-        for (int j = 0; j < 32; j++)
-        {
+    for (int i = 0; i < 128; i++) {
+        for (int j = 0; j < 32; j++) {
             printf("%02x ", MEM[j + i * 32]);
         }
         printf("\n");
--- main.cpp
+++ main.cpp
@@ -1,24 +1,27 @@
 #include <SDL2/SDL.h>
-#include <iostream>
+
 #include <chrono>
+#include <iostream>
 
 #include "chip8.h"
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 320;
-const int CPU_FREQ = 500;
 
-void drawGraphics(SDL_Window *window, CHIP8 chip8)
-{
+#ifdef SLOW
+const int CPU_FREQ = 10;
+#else
+const int CPU_FREQ = 500;
+#endif
+
+void drawGraphics(SDL_Window *window, CHIP8 chip8) {
     SDL_Surface *surface = SDL_GetWindowSurface(window);
     int scalex = surface->w / 64;
     int scaley = surface->h / 32;
 
-    for (int i = 0; i < 64; i++)
-    {
-        for (int j = 0; j < 32; j++)
-        {
+    for (int i = 0; i < 64; i++) {
+        for (int j = 0; j < 32; j++) {
             SDL_Rect rect;
             rect.w = scalex;
             rect.h = scaley;
@@ -31,60 +34,55 @@ void drawGraphics(SDL_Window *window, CHIP8 chip8)
     SDL_UpdateWindowSurface(window);
 }
 
-int keyToIndex(SDL_Event event)
-{
-    switch (event.key.keysym.sym)
-    {
-    case SDLK_1:
-        return 0;
-    case SDLK_2:
-        return 1;
-    case SDLK_3:
-        return 2;
-    case SDLK_4:
-        return 3;
-    case SDLK_q:
-        return 4;
-    case SDLK_w:
-        return 5;
-    case SDLK_e:
-        return 6;
-    case SDLK_r:
-        return 7;
-    case SDLK_a:
-        return 8;
-    case SDLK_s:
-        return 9;
-    case SDLK_d:
-        return 10;
-    case SDLK_f:
-        return 11;
-    case SDLK_z:
-        return 12;
-    case SDLK_x:
-        return 13;
-    case SDLK_c:
-        return 14;
-    case SDLK_v:
-        return 15;
-    default:
-        return 0;
+int keyToIndex(SDL_Event event) {
+    switch (event.key.keysym.sym) {
+        case SDLK_1:
+            return 1;
+        case SDLK_2:
+            return 2;
+        case SDLK_3:
+            return 3;
+        case SDLK_4:
+            return 0xC;
+        case SDLK_q:
+            return 4;
+        case SDLK_w:
+            return 5;
+        case SDLK_e:
+            return 6;
+        case SDLK_r:
+            return 0xD;
+        case SDLK_a:
+            return 7;
+        case SDLK_s:
+            return 8;
+        case SDLK_d:
+            return 9;
+        case SDLK_f:
+            return 0xE;
+        case SDLK_z:
+            return 0xA;
+        case SDLK_x:
+            return 0;
+        case SDLK_c:
+            return 0xB;
+        case SDLK_v:
+            return 0xF;
+        default:
+            return 0;
     }
 }
 
-int main()
-{
+int main(int argc, char *argv[]) {
     SDL_Window *window = NULL;
     SDL_Surface *screenSurface = NULL;
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return 0;
     }
 
     window = SDL_CreateWindow("CHIP-8 EMULATOR", 300, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (window == NULL)
-    {
+    if (window == NULL) {
         printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         return 0;
     }
@@ -96,25 +94,18 @@ int main()
 
     CHIP8 cpu;
     cpu.init();
-    cpu.loadApp("ROMS/pong2.c8");
+    cpu.loadApp(argv[1]);
 
     SDL_Event event;
     bool exit = false;
-    while (!exit)
-    {
+    while (!exit) {
         auto begin = std::chrono::high_resolution_clock::now();
-        while (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_QUIT)
-            {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
                 exit = true;
-            }
-            else if (event.type == SDL_KEYDOWN)
-            {
+            } else if (event.type == SDL_KEYDOWN) {
                 cpu.keys[keyToIndex(event)] = 1;
-            }
-            else if (event.type == SDL_KEYUP)
-            {
+            } else if (event.type == SDL_KEYUP) {
                 cpu.keys[keyToIndex(event)] = 0;
             }
         }
